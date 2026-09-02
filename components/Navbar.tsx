2c640e9792d1868bce9158c2deb1172e46953027
Azri-23 <azri4579@gmail.com>
2026-09-02 21:51:30 +0800

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,18 @@ import Link from "next/link";
 import { navLinks } from "@/types";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <ul>
-                {navLinks.map((link) => (
-                    <li key={link.href}>
-                        <Link href={link.href}>{link.label}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-sm">
+      <ul className="flex gap-6">
+        {navLinks.map((link) => (
+          <li
+            key={link.href}
+            className="text-gray-700 font-medium hover:text-blue-500"
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
